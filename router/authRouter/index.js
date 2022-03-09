da69e2287c186f46870e50e9206c1a39c2ff5972
gron1gh1 <gron1gh1@gmail.com>
2022-03-09 17:42:50 +0900

    로그인 기능 끝

--- router/authRouter/index.js
+++ router/authRouter/index.js
@@ -1,14 +1,56 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../model/User");
 
 // login / register
 
-router.post("/login", (req, res) => {
-  res.send("auth 라우터입니다");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const findUser = await User.findOne({
+      username: username,
+    });
+    if (!findUser) {
+      // 아이디 없음
+      res.json({
+        status: "fail",
+        message: "아이디가 존재하지 않습니다.",
+      });
+    } else if (findUser.password !== password) {
+      res.json({
+        status: "fail",
+        message: "비밀번호가 존재하지 않습니다.",
+      });
+    } else {
+      res.json({
+        status: "succ",
+      });
+    }
+  } catch (e) {
+    res.json({
+      status: "fail",
+      message: e,
+    });
+  }
 });
 
-router.post("/register", (req, res) => {
-  res.send("auth 라우터입니다");
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const makeUser = new User({
+      username,
+      password,
+    });
+    await makeUser.save();
+    res.json({
+      status: "succ",
+    });
+  } catch (e) {
+    res.json({
+      status: "fail",
+      message: e,
+    });
+  }
 });
 
 module.exports = router;
