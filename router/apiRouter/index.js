97ad2d695a220bb667b5b352a67aebb13d747518
gron1gh1 <gron1gh1@gmail.com>
2022-03-09 18:13:42 +0900

    jwt 토큰

--- router/apiRouter/index.js
+++ router/apiRouter/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const User = require("../../model/User");
 // 가입된 유저 리스트 (읽기)
+const loginRequired = require("../../middleware/loginRequired");
 
 // /api/user
+router.use(loginRequired);
+
 router.get("/user", async (req, res) => {
   try {
     const allUser = await User.find({}).select("username");
@@ -11,6 +14,12 @@ router.get("/user", async (req, res) => {
   } catch (e) {
     res.json({ status: "fail", message: e });
   }
+});
+
+router.get("/data", (req, res) => {
+  res.json({
+    data: "123",
+  });
 });
 
 module.exports = router;
